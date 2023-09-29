bd92c89ef1eb23a8b14e7d9ed34d468832b76b5b
Kalpesh Lambade <123794408+KalpeshLambade@users.noreply.github.com>
2023-09-29 15:40:53 +0530

    updated

--- testapp/lib/main.dart
+++ testapp/lib/main.dart
@@ -8,22 +8,76 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/cat.jpg'),
               ),
-              Text('Sleepy Cat',
-              style: TextStyle(
+              const Text(
+                'Sleepy cat',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'CAT DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSans',
+                    color: Colors.teal.shade100,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 3),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+91 12345 67890',
+                      style: TextStyle(
+                          fontFamily: 'SourceSans',
+                          color: Colors.teal.shade900,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w800),
+                    ),
+                  )),
+              Card(
                 color: Colors.white,
-                fontSize: 30.0,
-                fontWeight: FontWeight.w400
-              ),),
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'meowmeow@gamil.com',
+                    style: TextStyle(
+                        fontFamily: 'SourceSans',
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w800),
+                  ),
+                ),
+              )
             ],
           ),
         ),
